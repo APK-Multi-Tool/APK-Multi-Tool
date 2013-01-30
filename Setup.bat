2263378faef1a916e9b2217bac14e3e170ec9f71
Gerald Wayne Baggett Jr <raziel23x@gmail.com>
2013-01-29 20:09:30 -0500

    Moved DEPENDENCY FILES installtion
    to apkinstall.bat to clean up installation and hopefully fix the bat
    file closing after each install of the DEPENDENCY FILES  so we do not
    have to relaunch each and every time

--- Setup.bat
+++ Setup.bat
@@ -249,168 +249,21 @@ goto FRAMRES
 :FRAMRES1
 cls
 cd other
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                         Searching for framework-res.apk                       *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-cls
-IF NOT EXIST framework-res.apk (
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *        framework-res.apk not Found please make sure the file is in the        *
-ECHO *        other Directory returning to Dependencies menu                         *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
-IF EXIST framework-res.apk (
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                       framework-res.apk Found Installing                      *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-apktool if framework-res.apk
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                  Installation of framework-res.apk Complete                   *
-ECHO *                       Returning to Dependencies menu                          *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
+Start "Installation of framework-res.apk Starting" apkinstall 1
+goto FRAMRES
 :FRAMRES2
 cls
 cd other
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                        Searching for twframework-res.apk                      *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-IF NOT EXIST twframework-res.apk (
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *        twframework-res.apk not Found please make sure the file is in the      *
-ECHO *        other Directory returning to Dependencies menu                *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
-IF EXIST twframework-res.apk (
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                      twframework-res.apk Found Installing                     *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-cls
-apktool if twframework-res.apk
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                 Installation of twframework-res.apk Complete                  *
-ECHO *                       Returning to Dependencies menu                          *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
+Start "Installation of twframework-res.apk Starting" apkinstall 2
+goto FRAMRES
 :FRAMRES3
 cls
 cd other
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                       Searching for com.htc.resources.apk                     *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-IF NOT EXIST com.htc.resources.apk (
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *        com.htc.resources.apk not Found please make sure the file is in the    *
-ECHO *        other Directory returning to Dependencies menu                *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
-IF EXIST com.htc.resources.apk (
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                     com.htc.resources.apk Found Installing                    *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-apktool if com.htc.resources.apk
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                Installation of com.htc.resources.apk Complete                 *
-ECHO *                       Returning to Dependencies menu                          *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
+Start "Installation of com.htc.resources.apk Starting" apkinstall 3
+goto FRAMRES
 :FRAMRES4
-ping 1.1.1.1 -n 1 -w 2000 > nul
 cd other
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                            Searching for SystemUI.apk                         *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-cls
-IF NOT EXIST SystemUI.apk (
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *        SystemUI.apk not Found please make sure the file is in the             *
-ECHO *        other Directory returning to Dependencies menu                         *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul 
-cd "%~dp0"
-goto FRAMRESRET
-)
-IF EXIST SystemUI.apk (
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                         SystemUI.apk Found Installing                         *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-apktool if SystemUI.apk
-cls
-ECHO *********************************************************************************
-ECHO *                                                                               *
-ECHO *                     Installation of SystemUI.apk Complete                     *
-ECHO *                       Returning to Dependencies menu                          *
-ECHO *                                                                               *
-ECHO *********************************************************************************
-ping 1.1.1.1 -n 1 -w 2000 > nul
-cd "%~dp0"
-goto FRAMRESRET
-)
-:FRAMRESRET
+Start "Installation of SystemUI.apk Starting" apkinstall 4
 goto FRAMRES
 
 :ABOUT
